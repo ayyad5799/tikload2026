1e190d47c7a579db817077021fe9757bd4a447c5
ayyad5799 <mm3yad@gmail.com>
2026-03-28 14:31:14 +0300

    Update tikwm.js

--- api/tikwm.js
+++ api/tikwm.js
@@ -10,18 +10,15 @@ export default async function handler(req, res) {
   try {
     const params = new URLSearchParams(rest).toString();
     const url = `https://tiktok-scraper7.p.rapidapi.com/${endpoint || 'user/posts'}${params ? '?' + params : ''}`;
-
     const response = await globalThis.fetch(url, {
       headers: {
         'x-rapidapi-host': 'tiktok-scraper7.p.rapidapi.com',
         'x-rapidapi-key': RAPIDAPI_KEY,
       },
     });
-
     const data = await response.json();
     return res.status(200).json(data);
   } catch (e) {
     return res.status(500).json({ error: e.message, code: -1 });
   }
 }
- 
